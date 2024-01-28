70de8d4aecc079be01c6b96253162614b60782d4
Melvinensah <melvinbunduka@gmail.com>
2024-01-28 04:12:44 +0000

    updated

--- task3.js
+++ task3.js
@@ -1,42 +1,51 @@
-/* LESSON 3 - Programming Tasks */
+/* Lesson 3 */
 
 /* FUNCTIONS */
-/* Function Definition - Add Numbers */
-function add(number1, number2)
-{
+
+// Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
+// Step 2: In the function, return the sum of the parameters number1 and number2
+function add(number1, number2) {
     return number1 + number2;
 }
+// Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
+// Step 4: Assign the return value to an HTML form element with an ID of sum
+// Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
 
 function addNumbers() {
-    let num1 = parseInt(document.querySelector("#add1").value);
-    let num2 = parseInt(document.querySelector("#add2").value);
+    let num1 = parseInt(document.querySelector("#addend1").value);
+    let num2 = parseInt(document.querySelector("#addend2").value);
     document.getElementById("sum").value = add(num1, num2);
 }
-
 document.getElementById("addNumbers").addEventListener('click', addNumbers);
-/* Function Expression - Subtract Numbers */
-let subtract = funtion(number3, number4)
-{
+
+
+
+// Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
+function subtract(number3, number4) {
     return number3 - number4;
 }
 
-let subtractNumbers = function()
-{
-    let num3 = parseInt(document.querySelector("#subtract1").value);
-    let num4 = parseInt(document.querySelector("#subtract2").value);
-    document.getElementById("difference").value = subtract(num3, num4);  
+function subtractNumbers() {
+    let num3 = parseInt(document.querySelector("#minuend").value);
+    let num4 = parseInt(document.querySelector("#subtrahend").value);
+    document.getElementById("difference").value = subtract(num3, num4);
 }
-/* Arrow Function - Multiply Numbers */
+document.getElementById("subtractNumbers").addEventListener('click', subtractNumbers);
+
+
+
+// Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
+
 multiply = (number5, number6) => number5 * number6;
 multiplyNumbers = () => {
-    let num5 = Number(document.querySelector("#factor1").value);
-    let num6 = Number(document.querySelector("#factor2").value);
+    let num5 = parseInt(document.querySelector("#factor1").value);
+    let num6 = parseInt(document.querySelector("#factor2").value);
     document.getElementById("product").value = multiply(num5, num6);
 }
 document.getElementById("multiplyNumbers").addEventListener('click', multiplyNumbers);
 
 
-/* Open Function Use - Divide Numbers */
+// Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
 function divide(number7, number8) {
     return number7 / number8;
 }
@@ -49,11 +58,23 @@ function divideNumbers() {
 document.getElementById("divideNumbers").addEventListener('click', divideNumbers);
 
 
-/* Decision Structure */
+
+// Step 9: Test all of the mathematical functionality of the task3.html page.
 
 
-/* ARRAY METHODS - Functional Programming */
-/* Output Source Array */
+/* BUILT-IN METHODS */
+
+// Step 1: Declare and instantiate a variable of type Date to hold the current date
+let newDate = new Date();
+// Step 2: Declare a variable to hold the current year
+let currentYear;
+// Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
+currentYear = newDate.getFullYear();
+// Step 4: Assign the current year variable to an HTML form element with an ID of year
+document.getElementById("year").innerHTML = currentYear;
+
+/* ARRAY METHODS */
+
 // Step 1: Declare and instantiate an array variable to hold the numbers 1 through 25
 const array_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, ];
 // Step 2: Assi[gn the value of the array variable to the HTML element with an ID of "array"
