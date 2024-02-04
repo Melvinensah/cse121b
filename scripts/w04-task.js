5d20c851e4a86bb3536fc5d4341fe213cf97779b
Melvinensah <melvinbunduka@gmail.com>
2024-02-04 03:34:50 +0000

    woorking on week 4

---
+++ scripts/w04-task.js
@@ -0,0 +1,34 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+let myProfile = {
+    name : "Melvinensah Bunduka"
+    photo : "images/FB.img.png"
+    favoriteFoods : ["rice and krain-krain","jelly","foo-foo","friedrice and chicken"]
+    hobbies : ["playiing football","watching footabll match","scriptur study"]
+    pacesLived : []
+}
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
